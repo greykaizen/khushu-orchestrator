4ab651720c07c6df9e3f64620cd9ff411a7861d3
Syed Ali <shahaddy1@gmail.com>
2026-09-15 19:42:33 +0500

    chore: bump artifact version 1.6.0 -> 1.7.0 (com.khushu.data.store seam + positional Row)

--- orchestrator/build.gradle.kts
+++ orchestrator/build.gradle.kts
@@ -33,7 +33,7 @@ publishing {
             from(components["java"])
             groupId = "com.khushu"
             artifactId = "orchestrator"
-            version = "1.6.0"
+            version = "1.7.0"
             pom {
                 name.set("Khushu Orchestrator")
                 description.set(
